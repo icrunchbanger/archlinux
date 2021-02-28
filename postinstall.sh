369aac36fb7a1745b0365e0265486e3ca4ac614f
icrunchbanger <icrunchbanger@gmail.com>
2021-02-28 16:20:48 +0100

    update

--- postinstall.sh
+++ postinstall.sh
@@ -31,7 +31,7 @@ if (dialog --title "GIT Private Repo" --yesno "This part of the script clones pr
 
   then
 
-mkdir -pv /home/$USER/tmpr/keys
+mkdir -pv keys
 
 git_user=$(dialog --stdout --passwordbox "GIT username?" 0 0) || exit 1
 git_url=$(dialog --stdout --passwordbox "GIT URL Provider?" 0 0) || exit 1
@@ -48,6 +48,8 @@ for file in private/*.zip
     7z x "$file" -o/home/$USER/ -p"$git_repo_cr_pass"
   done
    fi
+
+mv */*.sh /home/$USER/.local/bin/
 
 rm -rf /home/$USER/{tmpr,packages.sh,postinstall.sh}
 
